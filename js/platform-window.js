56b4ca4c19fca91ab9a242f3595940492a12a629
Pierre Baize <pierre@openfin.co>
2020-07-28 07:27:46 -0600

    Add simple content creation rule configuration

--- js/platform-window.js
+++ js/platform-window.js
@@ -1,5 +1,8 @@
 export const CONTAINER_ID = 'layout-container';
 window.addEventListener('DOMContentLoaded', () => {
     // Before .50 AI version this may throw...
-    fin.Platform.Layout.init({containerId: CONTAINER_ID});
+    fin.Platform.Layout.init({ containerId: CONTAINER_ID });
+    fin.me.on('view-child-view-created', ({ childOptions }) => {
+        fin.Platform.getCurrentSync().createView(childOptions, fin.me.identity)
+    })
 });
